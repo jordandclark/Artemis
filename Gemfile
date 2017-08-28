eda93f7809ad7334594b2d3a74d69b83628f741e
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-08-28 16:54:12 -0700

    add jquery to next and back buttons for assessment

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'jquery-rails'
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
+  gem 'pry'
+
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- app/models/assessment.rb
+++ app/models/assessment.rb
@@ -1,2 +1,3 @@
 class Assessment < ApplicationRecord
+    validates :remoteOneValue,:remoteTwoValue,:remoteThreeValue, :remoteFourValue, :remoteFiveValue, :strategyOneValue, :strategyTwoValue, :strategyThreeValue, :strategyFourValue, :strategyFiveValue, :strategySixValue, presence: { message: "was left unanswered." }
 end
