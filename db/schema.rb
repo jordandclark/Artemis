a11467d1ca1b3a0cfb02dc5cbc1c3a49236a81f7
Aaron Nguyen <aaron3831@gmail.com>
2017-08-31 17:09:41 -0700

    add questions users join table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831235739) do
+ActiveRecord::Schema.define(version: 20170901000214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,6 +82,16 @@ ActiveRecord::Schema.define(version: 20170831235739) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "questions_users", id: false, force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "question_id", null: false
+    t.string "user_selection"
+    t.integer "user_score"
+    t.integer "section_number"
+    t.index ["question_id", "user_id"], name: "index_questions_users_on_question_id_and_user_id"
+    t.index ["user_id", "question_id"], name: "index_questions_users_on_user_id_and_question_id"
   end
 
   create_table "users", force: :cascade do |t|
