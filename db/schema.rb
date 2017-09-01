f9dd94c55dc8df33b18758d96f0c02d226ae3545
Aaron Nguyen <aaron3831@gmail.com>
2017-08-31 17:19:40 -0700

    add migration fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170901001424) do
+ActiveRecord::Schema.define(version: 20170901001740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "answers", force: :cascade do |t|
+    t.string "answer_text"
+    t.integer "answer_weight"
+  end
 
   create_table "answers_questions", id: false, force: :cascade do |t|
     t.bigint "answer_id", null: false
