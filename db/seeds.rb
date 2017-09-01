bff0bb6cfda75b26c8758d4b3788a05bcd05201d
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-09-01 12:15:22 -0700

    minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,22 +15,83 @@ class Seed
   end
 
   def make_questions
-    Question.create!(
+    # Question.create!(
+    #   section_number: 1,
+    #   question_text: 'Our CEO or another Senior Executive is a sponsor of diversity and inclusion.')
+    #     Answer.create!(
+    #     answer_text: 'We’ve taken no actions to address this.',
+    #     answer_weight: 1
+    #     )
+    #     Answer.create!(
+    #     answer_text: 'We taken actions to address this, but have not initiated a discussion.',
+    #     answer_weight: 2
+    #     )
+    #     Answer.create!(
+    #     answer_text: 'We’ve defined verbal and written standards to support this.',
+    #     answer_weight: 3
+    #     )
+    #     Answer.create!(
+    #     answer_text: 'We’ve defined standards and have integrated it into our workplace norms.',
+    #     answer_weight: 4
+    #     )
+    #     Answer.create!(
+    #     answer_text: 'We have standards and policies set to support this effort that represent best practice.',
+    #     answer_weight: 5
+    #     )
+
+
+
+      Question.create(
       section_number: 1,
-      question_text: 'Our CEO or another Senior Executive is a sponsor of diversity and inclusion.')
-    Question.create!(
-      section_number: 1,
-      question_text: 'We have a dedicated senior level employee overseeing and managing the company’s diversity performance.')
-    Question.create!(
-      section_number: 1,
-      question_text: 'We have a diversity/inclusion committee that meets on a regular basis.')
-    Question.create!(
-      section_number: 1,
-      question_text: 'We have a diversity and inclusion strategy with clear metrics and goals.')
-    Question.create!(
-      section_number: 1,
-      question_text: 'Our diversity and inclusion goals are integrated into the companyorganization’s strategic plan.')
-  end
+      question_text: 'Our CEO or another Senior Executive is a sponsor of diversity and inclusion.',
+      Question.Answers => [
+        {
+          answer_text: 'We’ve taken no actions to address this.',
+          answer_weight: 1
+        },
+        {
+          answer_text: 'We taken actions to address this, but have not initiated a discussion.',
+          answer_weight: 2
+        },
+        {
+          answer_text: 'We’ve defined verbal and written standards to support this.',
+          answer_weight: 3
+        },
+        {
+          answer_text: 'We’ve defined standards and have integrated it into our workplace norms.',
+          answer_weight: 4
+        },
+        {
+          answer_text: 'We have standards and policies set to support this effort that represent best practice.',
+          answer_weight: 5
+        }
+        ])
+    end
+
+
+
+    #
+    # Question.create!(
+    #   section_number: 1,
+    #   question_text: 'We have a dedicated senior level employee overseeing and managing the company’s diversity performance.')
+    # Question.create!(
+    #   section_number: 1,
+    #   question_text: 'We have a diversity/inclusion committee that meets on a regular basis.')
+    # Question.create!(
+    #   section_number: 1,
+    #   question_text: 'We have a diversity and inclusion strategy with clear metrics and goals.')
+    # Question.create!(
+    #   section_number: 1,
+    #   question_text: 'Our diversity and inclusion goals are integrated into the companyorganization’s strategic plan.')
+    # Question.create!(
+    #   section_number: 2,
+    #   question_text: 'Our talent management strategy links to the organization’s broader diversity and inclusion strategy and goals')
+    # Question.create!(
+    #   section_number: 2,
+    #   question_text: 'We are successful in recruiting and retaining Board leaders, senior management, and other leadership positions from diverse backgrounds with the expertise and ability to execute our strategic plan.')
+    # Question.create!(
+    #   section_number: 2,
+    #   question_text: 'All job descriptions include diversity and inclusion statements.')
 
 end
 
