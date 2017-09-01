0a9513e6fd1f2412e6d33483ee9cdb15661fdc04
Aaron Nguyen <aaron3831@gmail.com>
2017-09-01 12:14:43 -0700

    smooth out commit differences

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,10 @@
 class Question < ApplicationRecord
   has_many :responses
+  has_many :answer_questions
   has_many :respondents, through: :responses
   has_many :answers, through: :answer_questions
 end
+
+user > user_selection > question > answer_id > answer_text
+
+user > user_score > question > answer_id > answer_weight
