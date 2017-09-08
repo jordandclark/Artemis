d7198702905213aa937ab9224a4a1dcf7e54cd3a
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-09-08 11:07:27 -0700

    change style on name page

--- db/schema.rb
+++ db/schema.rb
@@ -127,6 +127,11 @@ ActiveRecord::Schema.define(version: 20170908000022) do
     t.index ["respondent_id"], name: "index_responses_on_respondent_id"
   end
 
+  create_table "user_questions", force: :cascade do |t|
+    t.integer "respondent_id"
+    t.integer "question_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
