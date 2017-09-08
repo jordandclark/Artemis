ab74e025e34d078c61e8ecc9623f0ec41493a680
Brian Fan <kuomu.fan@yahoo.com>
2017-09-08 16:23:44 -0700

    add line style back

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908000022) do
+ActiveRecord::Schema.define(version: 20170908225500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -123,6 +123,9 @@ ActiveRecord::Schema.define(version: 20170908000022) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "answer_hash"
+    t.text "one_hash"
+    t.text "two_hash"
+    t.text "three_hash"
     t.index ["question_id"], name: "index_responses_on_question_id"
     t.index ["respondent_id"], name: "index_responses_on_respondent_id"
   end
