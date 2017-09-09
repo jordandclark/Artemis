db80904bfc98a8f3a955685272ef035768153ac3
Brian Fan <kuomu.fan@yahoo.com>
2017-09-09 13:49:15 -0700

    add diversity_intro page

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,25 @@
 Rails.application.routes.draw do
 
+  root :to => 'welcomes#index'
+  resources :welcomes, only: [:index]
+
+  # Devise
+  devise_for :users
+
+  # Hiring Quiz
+  resources :assessments, except: [:new]
+  resources :hiring_results, only: [:index]
+  get '/quiz_intro', to: 'assessments#intro'
+  get '/hiring_quiz', to: 'assessments#new'
+
   resources :respondents do
     resources :responses
   end
 
-  root :to => 'welcomes#index'
-
-  resources :welcomes, only: [:index]
-
-  resources :hiring_results, only: [:index]
+  # D & I assessments
   resources :diversity_results, only: [:index]
-
-  devise_for :users
-  resources :diversities
-
-  # Hiring Quiz
-  resources :assessments, except: [:new]
-
-  get '/quiz_intro', to: 'assessments#intro'
-  get '/hiring_quiz', to: 'assessments#new'
-
-
-
-
-
-
+  resources :diversities, except: [:new]
+  get '/diversity_intro', to: 'diversities#intro'
+  get '/diversity_assessment', to: 'diversities#new'
 
 end
